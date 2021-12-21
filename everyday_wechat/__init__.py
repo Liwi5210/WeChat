f1f543b2cd5fa3c3ea234722e6e09b159de86552
Liwi_Liwi <87483276+Liwi5210@users.noreply.github.com>
2021-12-21 15:10:28 +0800

    Create __init__.py

---
+++ everyday_wechat/__init__.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python
+# -*- coding: utf-8 -*-
+# Date: 2021-12-21
+# Author: Liwi
+
+script_name = 'EverydayWechat'
+__author__ = 'sfyc23'
+__license__ = 'MIT'
+__version__ = '0.3.27'
